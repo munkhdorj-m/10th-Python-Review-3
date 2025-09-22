6c29a286d11040331d62348c627d90df5b7855ce
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2025-09-22 23:25:44 +0800

    Initial commit

---
+++ assignment.py
@@ -0,0 +1,25 @@
+# You can remove 'pass' if you written code in the function 
+# Exercise 1
+def print_even_numbers():
+    # Write your code here
+    pass
+
+# Exercise 2
+def reverse_number(num):
+    # Write your code here
+    pass
+
+# Exercise 3
+def sum_of_n_numbers(num):
+    # Write your code here
+    pass
+
+# Exercise 4
+def is_prime(num):
+    # Write your code here
+    pass
+
+# Exercise 5
+def print_perfect_squares():
+    # Write your code here
+    pass

684fb3b8c9d65ad2ddf961e029aa229eecb65622
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2025-09-22 23:31:31 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,25 +1,22 @@
 # You can remove 'pass' if you written code in the function 
 # Exercise 1
-def print_even_numbers():
+
+def count_digits():
     # Write your code here
     pass
 
 # Exercise 2
-def reverse_number(num):
+def factorial(num):
     # Write your code here
     pass
 
 # Exercise 3
-def sum_of_n_numbers(num):
+def primes_up_to(num):
     # Write your code here
     pass
 
 # Exercise 4
-def is_prime(num):
+def average_of_list(num):
     # Write your code here
     pass
 
-# Exercise 5
-def print_perfect_squares():
-    # Write your code here
-    pass
